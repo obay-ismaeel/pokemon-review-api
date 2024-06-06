c31d6b2eb1abb52584509a1fc1ae4aecea13bcb6
Obay Ismaeel <obayhany@gmail.com>
2024-06-06 14:17:30 +0300

    Created Unit of Work interface and class

---
+++ Abstractions/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using PokemonReviewApp.Models;
+using PokemonReviewApp.Repositories;
+
+namespace PokemonReviewApp.Abstractions;
+
+public interface IUnitOfWork
+{
+    IUserRepository Users { get; }
+    IPokemonRepository Pokemons { get; }
+    IReviewRepository Reviews { get; }
+    IReviewerRepository Reviewers { get; }
+    ICountryRepository Countries { get; }
+    ICategoryRepository Categories { get; }
+    IOwnerRepository Owners { get; }
+    Task<int> CompletAsync();
+    void Dispose();
+}
---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using PokemonReviewApp.Abstractions;
+using PokemonReviewApp.Repositories;
+
+namespace PokemonReviewApp.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        Users = new UserRepository(context);
+        Pokemons = new PokemonRepository(context);
+        Owners = new OwnerRepository(context);
+        Reviewers = new ReviewerRepository(context);
+        Reviews = new ReviewRepository(context);
+        Countries = new CountryRepository(context);
+        Categories = new CategoryRepository(context);
+    }
+
+    public IUserRepository Users { get; private set; }
+
+    public IPokemonRepository Pokemons { get; private set; }
+
+    public IReviewRepository Reviews { get; private set; }
+
+    public IReviewerRepository Reviewers { get; private set; }
+
+    public ICountryRepository Countries { get; private set; }
+
+    public ICategoryRepository Categories { get; private set; }
+
+    public IOwnerRepository Owners { get; private set; }
+
+    public async Task<int> CompletAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
